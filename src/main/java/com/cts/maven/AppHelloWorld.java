9bebbdfbc1715b954e74ef09bc7147cb32d97ac5
vinodramr <vinodram.ramavath@gmail.com>
2016-03-08 17:43:25 +0530

    Commit 7, Check for Polling using Jenkins

--- src/main/java/com/cts/maven/AppHelloWorld.java
+++ src/main/java/com/cts/maven/AppHelloWorld.java
@@ -5,7 +5,9 @@ public class AppHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World..");
-		System.out.println("Pushing change to github with Auto uils using jenkins");
+		System.out.println("Pushing change to github using jenkins");
+		
+		System.out.println("Pushing New changes to github with Polling in Jenkins");
 		
 		
 	}
