e73fbd4350e7b46c069f41c0a39a2ac930682868
vinodram <vinodram.ramavath@gmail.com>
2016-03-08 15:11:42 +0530

    second commit - Added ;

--- src/main/java/com/cts/maven/AppHelloWorld.java
+++ src/main/java/com/cts/maven/AppHelloWorld.java
@@ -5,7 +5,7 @@ public class AppHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World..");
-		System.out.println("Testing Git and Eclipse");
+		System.out.println("Testing Git and Eclipse");;
 	}
 
 }
