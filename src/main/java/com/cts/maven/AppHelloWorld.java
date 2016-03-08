19900793d0951357ef46589e65254b40f76db56e
vinodram <vinodram.ramavath@gmail.com>
2016-03-08 15:21:55 +0530

    added 3rd line

--- src/main/java/com/cts/maven/AppHelloWorld.java
+++ src/main/java/com/cts/maven/AppHelloWorld.java
@@ -5,7 +5,8 @@ public class AppHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World..");
-		System.out.println("Testing Git and Eclipse");;
+		System.out.println("Testing Git and Eclipse");
+		System.out.println("Testing Git and pushing");
 	}
 
 }
