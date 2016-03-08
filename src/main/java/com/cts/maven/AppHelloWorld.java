d8f0e20b9fe5997f3bfedd4c8853b0cc818b435b
vinodramr <vinodram.ramavath@gmail.com>
2016-03-08 14:47:37 +0530

    First Commit - checking eclipse and git

---
+++ src/main/java/com/cts/maven/AppHelloWorld.java
@@ -0,0 +1,11 @@
+package com.cts.maven;
+
+public class AppHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World..");
+		System.out.println("Testing Git and Eclipse");
+	}
+
+}
